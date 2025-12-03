6e6bebd158fed7fbf8e46ccf97b409c72b2679ef
Sujal-thool <sujalthool01@gmail.com>
2025-12-03 17:17:49 +0530

    yu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://gamified-education-app-sih-main.onrender.com',
         changeOrigin: true,
         secure: false,
       }
